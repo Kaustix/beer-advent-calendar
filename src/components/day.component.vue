b3b60340abfb2741c267ef08a10a814494d8807c
Kiernan Lamarche <kiernan.lamarche@gmail.com>
2020-11-17 16:58:35 -0700

    added advent calendar doors

--- src/components/day.component.vue
+++ src/components/day.component.vue
@@ -1,5 +1,5 @@
 <template>
-    <card class="container">
+    <card class="container" v-if="isDayVisible()">
         <template v-slot:header>
             <advent :day="beer.day" :person="beer.person" />
         </template>
@@ -7,22 +7,34 @@
             <beer :beer="beer" />
         </template>
     </card>
+    <div class="container" v-else>
+        <door :day="beer.day" />
+    </div>
 </template>
 
 <script>
 import card from '@/components/shared/card.component';
 import advent from '@/components/advent.component';
 import beer from '@/components/beer.component';
+import door from '@/components/door.component';
 
 export default {
     name: 'day',
     components: {
         card,
         advent,
-        beer
+        beer,
+        door
     },
     props: {
         beer: Object
+    },
+    methods: {
+        isDayVisible() {
+            const day = new Date().getDate();
+            const month = new Date().getMonth();
+            return month === 11 && this.beer.day <= day;
+        }
     }
 }
 </script>
---
+++ src/components/door.component.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="door">
+        <div class="text">Day {{day}}</div>
+        <div><img :src="require('../assets/santa-beer.png')" alt="santa-beer" class="image"/></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'door',
+    props: {
+        day: Number
+    }
+}
+</script>
+
+<style lang="less" scoped>
+     .door {
+        border-radius: 16px;
+        background: #f7f7f7;
+        font-family: 'Mountains of Christmas', cursive;
+        text-align: center;
+        font-size: 50px;
+
+        display: flex;
+        flex-direction: column;
+        align-content: center;
+        justify-items: center;
+    }
+
+    .image {
+        width: 200px;
+    }
+</style>
